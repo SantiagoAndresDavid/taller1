86d33d3b81efc8c5142598ba760d0cdeb5be24f6
santiago <sadavid@unicesar.edu.co>
2021-08-23 15:40:23 -0500

    Cambiar estructura del proyecto

---
+++ Taller1/src/Taller1/Soluciones/Punto1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Taller1.Soluciones
+{
+    public class Punto1
+    {
+        public string Solucion()
+        {
+            Console.Write("ingrese la primera nota: ");
+            int primeraNota = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese la segunda nota: ");
+            int segundaNota = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese la tercera nota: ");
+            int terceraNota = Convert.ToInt32(Console.ReadLine());
+
+            if ((primeraNota + segundaNota + terceraNota) / 3 >= 3)
+            {
+                return "el estudiante pudo pasar";
+            }
+
+            return "el estudiante no pudo pasar";
+        }
+    }
+}
---
+++ Taller1/src/Taller1/Soluciones/Punto2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Taller1.Soluciones
+{
+    public class Punto2
+    {
+        public int Solucion()
+        {
+            Console.Write("es hombre o mujer: ");
+            string sexo = Console.ReadLine();
+            Console.Write("cual es la edad: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            if (sexo.ToLower().Equals("hombre"))
+            {
+                return (210 - edad) / 10;
+            }
+
+            return (220 - edad) / 10;
+        }
+    }
+}
